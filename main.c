ac20580be2d488911bcc254876f4e1ff993f7a20
YoussefBA10 <99603917+YoussefBA10@users.noreply.github.com>
2022-02-14 16:22:43 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+#include"SDL/SDL.h"
+#include"SDL/SDL_image.h"
+#include"SDL/SDL_ttf.h"
+#include"SDL/SDL_mixer.h"
+
+int main(int argc,char *argv[]) {
+
+
+int volume_music=100;
+    SDL_Init(SDL_INIT_VIDEO);
+Mix_OpenAudio(22050,MIX_DEFAULT_FORMAT,2,1024);//1 -> mono ** 2 -> sterio
+Mix_Music *music;
+music= Mix_LoadMUS("menu/background.mp3");
+Mix_VolumeMusic(volume_music);
+Mix_PlayMusic(music,-1);//-1 play till the program ends ** n number of times
+
+
+//fenetre principale
+SDL_Surface *screen ;
+screen=SDL_SetVideoMode(1920,1080,32,SDL_HWSURFACE|SDL_DOUBLEBUF);
+SDL_WM_SetCaption("Unlifted Curse",NULL);
+iscntrl(screen);
+
+
+return 0 ;
+}

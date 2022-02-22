f939140cd00e6a294aeac6ed305524a2a52415ad
YoussefBA10 <99603917+YoussefBA10@users.noreply.github.com>
2022-02-22 22:49:22 +0100

    Update header.h

--- header.h
+++ header.h
@@ -5,12 +5,14 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_mixer.h>
-#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_ttf.h> 
 #include <string.h>
 
-int vol ;
-int done ;
+/*int vol ;
+int done ;*/
 int menu(SDL_Surface *screen,int vol);
 int options(SDL_Surface *screen,int vol);
+int Story(SDL_Surface *screen);
+int quit(SDL_Surface *screen);
 
 #endif // HEADER_H_INCLUDED
